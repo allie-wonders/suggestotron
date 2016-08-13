21ff2968b03bb91f9c2215973a33bfa0b43ae990
Allie O'Connell <allie.oconnell@gmail.com>
2016-08-13 12:58:05 -0400

    Added downvoting feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160813150635) do
+ActiveRecord::Schema.define(version: 20160813161231) do
+
+  create_table "downvotes", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "topics", force: :cascade do |t|
     t.string   "title"
